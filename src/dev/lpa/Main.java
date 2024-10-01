42fe7d37254f1a154306fa6c0bf33394c49f35d7
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-01 14:58:32 +0200

    add record Person

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,22 @@
 package dev.lpa;
 
+import java.util.Random;
+
+record Person(String firstName, String lastName, int age) {
+
+  private final static String[] firsts =
+    {"Able", "Bob", "Charlie", "Donna", "Eve", "Fred"};
+  private final static String[] lasts =
+    {"Norton", "Ohara", "Peterson", "Quincy", "Richardson", "Smith"};
+
+  private final static Random random = new Random();
+
+  public Person() {
+    this(firsts[random.nextInt(firsts.length)],
+      lasts[random.nextInt(lasts.length)],
+      random.nextInt(18, 100));
+  }
+}
 public class Main {
 
   public static void main(String[] args) {
