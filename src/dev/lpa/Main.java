9a68be435753b3e85f666e49d858aaced16c7247
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-01 19:32:03 +0200

    .collect(Collectors.joining(" ")) thread safe string join

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -67,6 +68,11 @@ public class Main {
     words.forEach(System.out::println);
     System.out.println("------------------------------");
 
+    var backTogetherAgain = words
+      .parallelStream()
+//      .reduce("", (st, s) ->  st += s + " ", String::concat);
+      .collect(Collectors.joining(" "));
 
+    System.out.println(backTogetherAgain);
   }
 }
