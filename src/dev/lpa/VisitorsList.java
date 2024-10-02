8113d7d4595eaabf578f272e8ff5a830e8957a0b
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-02 15:34:58 +0200

    Âadd consumer threadpool using poll

--- src/dev/lpa/VisitorsList.java
+++ src/dev/lpa/VisitorsList.java
@@ -6,12 +6,21 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 public class VisitorsList {
+
+  private static final CopyOnWriteArrayList<Person> masterList;
+
+  static {
+    masterList = Stream.generate(Person::new)
+      .distinct()
+      .limit(2500)
+      .collect(CopyOnWriteArrayList::new,
+        CopyOnWriteArrayList::add,
+        CopyOnWriteArrayList::addAll);
+  }
 
   // use when you want to control the number of elements in some fashion
   private static final ArrayBlockingQueue<Person> newVisitors =
@@ -22,7 +31,7 @@ public class VisitorsList {
     Runnable producer = () -> {
 
       Person visitor = new Person();
-      System.out.println("Adding " + visitor);
+      System.out.println("Queueing " + visitor);
       boolean queued = false;
       try {
         queued = newVisitors.offer(visitor, 5, TimeUnit.SECONDS);
@@ -30,7 +39,7 @@ public class VisitorsList {
         throw new RuntimeException(e);
       }
       if (queued) {
-        System.out.println(newVisitors);
+//        System.out.println(newVisitors);
       } else {
         System.out.println("Queue is Full, can not add " + visitor);
         System.out.println("Draining Queue and writing data to file");
@@ -49,15 +58,33 @@ public class VisitorsList {
       }
     };
 
+    Runnable consumer = () -> {
+      String threadName = Thread.currentThread().getName();
+      System.out.println(threadName + " Polling queue " + newVisitors.size());
+      Person visitor = newVisitors.poll();
+      if (visitor != null) {
+        System.out.println(threadName + " " +  visitor);
+        if (!masterList.contains(visitor)) {
+          masterList.add(visitor);
+          System.out.println("--> New Visitor gets Coupon!: " + visitor);
+        }
+      }
+    };
+
     ScheduledExecutorService producerExecutor =
       Executors.newSingleThreadScheduledExecutor();
 
     producerExecutor.scheduleWithFixedDelay(producer, 0 , 1,
       TimeUnit.SECONDS);
 
+    ScheduledExecutorService consumerPool = Executors.newScheduledThreadPool(3);
+    for (int i = 0; i < 3; i++) {
+      consumerPool.scheduleAtFixedRate(consumer, 6, 3, TimeUnit.SECONDS);
+    }
+
     while (true) {
       try {
-        if (!producerExecutor.awaitTermination(20, TimeUnit.SECONDS)) {
+        if (!producerExecutor.awaitTermination(10, TimeUnit.SECONDS)) {
           break;
         }
       } catch (InterruptedException e) {
@@ -66,5 +93,17 @@ public class VisitorsList {
     }
 
     producerExecutor.shutdown();
+
+    while (true) {
+      try {
+        if (!consumerPool.awaitTermination(3, TimeUnit.SECONDS)) {
+          break;
+        }
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    consumerPool.shutdown();
   }
 }
